227da867938f068965b006d8b2ad24254a889b2e
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-15 22:42:46 +0200

    neo4j config

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,11 +1,13 @@
 import { config } from "dotenv";
 config();
 
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { get } from "env-var";
 import { AppConfig } from "./app.config";
 import { DbConfig } from "./db.config";
+import { Neo4jConfig } from "./neo4j.config";
 
+@Global()
 @Module({
     providers: [{
         provide: AppConfig,
@@ -23,7 +25,14 @@ import { DbConfig } from "./db.config";
             get("DB_NAME").required().asString(),
             get("DB_PORT").required().asPortNumber()
         )
+    }, {
+        provide: Neo4jConfig,
+        useFactory: () => new Neo4jConfig(
+            get("NEO4J_URL").required().asUrlString(),
+            get("NEO4J_USER").required().asString(),
+            get("NEO4J_PASSWORD").required().asString()
+        )
     }],
-    exports: [AppConfig, DbConfig]
+    exports: [AppConfig, DbConfig, Neo4jConfig]
 })
 export class ConfigModule {}
---
+++ src/config/neo4j.config.ts
@@ -0,0 +1,7 @@
+export class Neo4jConfig {
+    constructor(
+        readonly URL: string,
+        readonly USER: string,
+        readonly PASSWORD: string
+    ) {}
+}
