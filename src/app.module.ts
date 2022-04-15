9c3e1e2dc8ec02402199565033d40b68ca473604
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-15 22:47:48 +0200

    Neo4j module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,37 +3,47 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { Dialect } from "sequelize/types";
 import { ConfigModule } from "./config/config.module";
 import { DbConfig } from "./config/db.config";
+import { Neo4jModule } from "./infrastructure/neo4j/neo4j.module";
 import { AccountModule } from "./usecases/accounts/account.module";
 import { AccountModel } from "./usecases/accounts/infrastructure/account.model";
 import { AuthModule } from "./usecases/auth/auth.module";
 import { ProfileModule } from "./usecases/profiles/profile.module";
 
+const persistence = [
+    SequelizeModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (config: DbConfig) => ({
+            dialect: config.DIALECT as Dialect,
+            host: config.HOST,
+            port: config.PORT,
+            username: config.USER,
+            password: config.PASSWORD,
+            database: config.NAME,
+            models: [AccountModel],
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+            logging: false
+        }),
+        inject: [DbConfig]
+    }),
+    Neo4jModule
+];
+
+const contexts = [
+    AccountModule, 
+    AuthModule,
+    ProfileModule
+];
+
 @Module({
     imports: [
         ConfigModule, 
-        AccountModule, 
-        AuthModule,
-        ProfileModule,
-        SequelizeModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (config: DbConfig) => ({
-                dialect: config.DIALECT as Dialect,
-                host: config.HOST,
-                port: config.PORT,
-                username: config.USER,
-                password: config.PASSWORD,
-                database: config.NAME,
-                models: [AccountModel],
-                dialectOptions: {
-                    ssl: {
-                        require: true,
-                        rejectUnauthorized: false
-                    }
-                },
-                logging: false
-            }),
-            inject: [DbConfig]
-        })
+        ...contexts,
+        ...persistence
     ]
 })
 export class AppModule {}
