8f9be8303465a9182eda156fc70cf76d09d1b7c9
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-16 01:46:38 +0200

    exported likes module

--- src/usecases/Social/social.module.ts
+++ src/usecases/Social/social.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { CommentsModule } from "./comments/comments.module";
+import { LikesModule } from "./likes/likes.module";
 import { PublicationsModule } from "./publications/publications.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-    imports: [UsersModule, PublicationsModule, CommentsModule]
+    imports: [UsersModule, PublicationsModule, CommentsModule, LikesModule]
 })
 export class SocialModule {}
