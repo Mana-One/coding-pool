fbb27e4f56cf6ffa9b19f8355257bf4e610de6ab
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-24 18:55:57 +0200

    replace program content route

--- src/modules/Programs/exposition/programs.controller.ts
+++ src/modules/Programs/exposition/programs.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, Get, Post, Query, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, Query, Req } from "@nestjs/common";
+import { request } from "http";
 import { AppConfig } from "../../../config/app.config";
 import { PageRequest } from "../../../kernel/PageRequest";
 import { PageResponse } from "../../../kernel/PageResponse";
 import { Public } from "../../auth/public.decorator";
 import { CreateProgramUsecase } from "../application/create-program/create-program.usecase";
 import { ListProgramsUsecase } from "../application/list-programs/list-programs.usecase";
+import { ReplaceContentUsecase } from "../application/replace-content/replace-content.usecase";
 import { CreateProgramRequest } from "./create-program.request";
 import { ListProgramsRequest } from "./list-programs.request";
+import { ReplaceContentRequest } from "./replace-content.request";
 
 @Controller("programs")
 export class ProgramsController {
     constructor(
         private readonly appConfig: AppConfig,
         private readonly createProgramUsecase: CreateProgramUsecase,
-        private readonly listProgramsUsecase: ListProgramsUsecase
+        private readonly listProgramsUsecase: ListProgramsUsecase,
+        private readonly replaceContentUsecase: ReplaceContentUsecase
     ) {}
 
     @Post()
@@ -25,6 +29,19 @@ export class ProgramsController {
             title: request.title,
             authorId: request.user.accountId,
             languageId: body.languageId
+        });
+    }
+
+    @Put(":id")
+    async replaceContent(
+        @Req() request,
+        @Param("id") id: string,
+        @Body() body: ReplaceContentRequest
+    ) {
+        await this.replaceContentUsecase.execute({
+            id,
+            content: body.content,
+            callerId: request.user.accountId
         });
     }
 
--- src/modules/Programs/infrastructure/programs.module.ts
+++ src/modules/Programs/infrastructure/programs.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { CreateProgramUsecase } from "../application/create-program/create-program.usecase";
 import { ListProgramsUsecase } from "../application/list-programs/list-programs.usecase";
+import { ReplaceContentUsecase } from "../application/replace-content/replace-content.usecase";
 import { PROGRAMS } from "../constants";
 import { ProgramsController } from "../exposition/programs.controller";
 import { SequelizePrograms } from "./sequelize.programs";
@@ -8,7 +9,8 @@ import { SequelizePrograms } from "./sequelize.programs";
 @Module({
     providers: [
         CreateProgramUsecase, 
-        ListProgramsUsecase, {
+        ListProgramsUsecase, 
+        ReplaceContentUsecase, {
         provide: PROGRAMS,
         useClass: SequelizePrograms
     }],
