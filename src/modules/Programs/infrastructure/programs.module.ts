23be4acfe0b037ea915f398fa4eb444c4afff1d5
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-24 18:36:44 +0200

    updated programs module

--- src/modules/Programs/infrastructure/programs.module.ts
+++ src/modules/Programs/infrastructure/programs.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
 import { CreateProgramUsecase } from "../application/create-program/create-program.usecase";
+import { ListProgramsUsecase } from "../application/list-programs/list-programs.usecase";
 import { PROGRAMS } from "../constants";
 import { ProgramsController } from "../exposition/programs.controller";
 import { SequelizePrograms } from "./sequelize.programs";
 
 @Module({
-    providers: [CreateProgramUsecase, {
+    providers: [
+        CreateProgramUsecase, 
+        ListProgramsUsecase, {
         provide: PROGRAMS,
         useClass: SequelizePrograms
     }],
