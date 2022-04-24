0371ba789d9558eb49ec1d0b68aeb5fec68cb96a
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-04-24 18:24:57 +0200

    list programs

--- src/modules/Programs/application/list-programs/list-programs.usecase.ts
+++ src/modules/Programs/application/list-programs/list-programs.usecase.ts
@@ -1,11 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { Usecase } from "../../../../kernel/Usecase";
+import { ProgramModel } from "../../infrastructure/program.model";
 import { ListProgramsQuery } from "./list-programs.query";
-import { Portfolio } from "./portfolio";
+import { Portfolio, SingleProgramDto } from "./portfolio";
 
 @Injectable()
 export class ListProgramsUsecase implements Usecase<ListProgramsQuery, Portfolio> {
-    execute(request: ListProgramsQuery): Promise<Portfolio> {
-        throw new Error("Method not implemented.");
+    async execute(request: ListProgramsQuery): Promise<Portfolio> {
+        const { rows, count } = await ProgramModel.findAndCountAll({
+            where: { authorId: request.authorId },
+            order: [["createdAt", "DESC"]],
+            limit: request.limit,
+            offset: request.offset
+        })
+        .catch(err => { throw new InternalServerErrorException(String(err)); });
+        return new Portfolio(rows.map(this.toDto), count, request.limit, request.offset);
+    }
+
+    private toDto(instance: ProgramModel): SingleProgramDto {
+        return {
+            id: instance.id,
+            title: instance.title,
+            createdAt: instance.createdAt
+        };
     }
 }
--- src/modules/Programs/application/list-programs/portfolio.ts
+++ src/modules/Programs/application/list-programs/portfolio.ts
@@ -3,6 +3,7 @@ import { Page } from "../../../../kernel/Page";
 export interface SingleProgramDto {
     id: string 
     title: string
+    createdAt: Date
 }
 
 export class Portfolio extends Page<SingleProgramDto> {}
