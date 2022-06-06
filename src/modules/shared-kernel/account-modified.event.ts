02d4380d823f4809d5723a011ee0c3f68bebf625
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-06-06 17:01:46 +0200

    account modified event

---
+++ src/modules/shared-kernel/account-modified.event.ts
@@ -0,0 +1,7 @@
+export class AccountModified {
+    constructor(
+        readonly id: string,
+        readonly username: string,
+        readonly email: string
+    ) {}
+}
--- src/modules/shared-kernel/constants.ts
+++ src/modules/shared-kernel/constants.ts
@@ -1,2 +1,3 @@
 export const ACCOUNT_CREATED_EVENT = Symbol.for("account.created");
 export const PROGRAM_CREATED_EVENT = Symbol.for("program.created");
+export const ACCOUNT_MODIFIED_EVENT = Symbol.for("account.modified");
