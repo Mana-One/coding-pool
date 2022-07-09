8f79df7dd9f1f2f4298c0b8f3c4ccb04615e4d69
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-07-09 11:40:24 +0200

    save last proposition

--- src/modules/CompetitiveMode/application/submissions/create-submission/create-submission.usecase.ts
+++ src/modules/CompetitiveMode/application/submissions/create-submission/create-submission.usecase.ts
@@ -3,8 +3,10 @@ import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import { UID } from "../../../../../kernel/UID";
 import { Usecase } from "../../../../../kernel/Usecase";
-import { CODE_JUDGE, COMPETITIONS } from "../../../constants";
+import { CODE_JUDGE, COMPETITIONS, LAST_PROPOSITIONS } from "../../../constants";
 import { Competitions } from "../../../domain/competitions/competitions";
+import { LastProposition } from "../../../domain/last-proposition/last-proposition";
+import { LastPropositions } from "../../../domain/last-proposition/last-propositions";
 import { CodeJudge } from "../code-judge";
 import { CreateSubmissionCommand } from "./create-submission.command";
 import { CreateSubmissionView } from "./create-submission.view";
@@ -13,6 +15,7 @@ import { CreateSubmissionView } from "./create-submission.view";
 export class CreateSubmissionUsecase implements Usecase<CreateSubmissionCommand, CreateSubmissionView> {
     constructor(
         @Inject(COMPETITIONS) private readonly competitions: Competitions,
+        @Inject(LAST_PROPOSITIONS) private readonly lastPropositions: LastPropositions,
         @Inject(CODE_JUDGE) private readonly codeJudge: CodeJudge
     ) {}
 
@@ -26,6 +29,17 @@ export class CreateSubmissionUsecase implements Usecase<CreateSubmissionCommand,
         if (O.isNone(competition)) {
             throw new NotFoundException("Competition not found.");
         }
+
+        const lastProposition = LastProposition.create({
+            competitionId: request.competitionId,
+            participantId: request.participantId,
+            sourceCode: request.source_code
+        });
+        if (E.isLeft(lastProposition)) {
+            throw new BadRequestException(lastProposition.left.join("\n"));
+        }
+
+        await this.lastPropositions.save(lastProposition.right);
 
         return this.codeJudge.send({
             competitionId: request.competitionId,
--- src/modules/CompetitiveMode/constants.ts
+++ src/modules/CompetitiveMode/constants.ts
@@ -1,3 +1,4 @@
 export const COMPETITIONS = Symbol.for("competitions");
 export const CODE_JUDGE = Symbol.for("code_judge");
 export const SUBMISSIONS = Symbol.for("submissions");
+export const LAST_PROPOSITIONS = Symbol.for("last_propositions");
